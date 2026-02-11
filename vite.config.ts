4b18a30efdb16e968b26cf7b996095a4e63a1394
kang <lowkangxuan@gmail.com>
2026-02-11 12:48:09 +0800

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
+// import netlify from "@netlify/vite-plugin-tanstack-start";
 import contentCollections from "@content-collections/vite";
 import { nitro } from "nitro/vite";
 
