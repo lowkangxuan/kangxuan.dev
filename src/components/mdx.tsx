37091d591b02f9e8449dfeb1100614532e2a3727
kang <lowkangxuan@gmail.com>
2026-03-15 14:09:21 +0800

    Minor changes to styling

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -2,11 +2,12 @@ import { MDXContent } from "@content-collections/mdx/react";
 import { CodeBlock } from "@/components/code-block.tsx";
 
 const components = {
-    h1: (props) => <h1 className="font-inter text-3xl font-semibold mt-12 mb-6" {...props} />,
-    h2: (props) => <h2 className="font-inter text-2xl font-semibold mt-12 mb-6" {...props} />,
+    h1: (props) => <h1 className="font-geist text-3xl font-semibold mt-12 mb-4" {...props} />,
+    h2: (props) => <h2 className="font-geist text-2xl font-semibold mt-12 mb-4" {...props} />,
+    h3: (props) => <h3 className="font-geist text-xl font-semibold mt-12 mb-4" {...props} />,
     pre: (props) => <CodeBlock props={{...props}} />,
     a: (props) => <a className="font-semibold underline" {...props} />,
-    p: (props) => <p className="font-inter first:mt-0 my-5" {...props} />,
+    p: (props) => <p className="font-geist first:mt-0 my-5" {...props} />,
     ul: (props) => <ul className="text-primary pl-6" {...props} />,
     li: (props) => <li className="text-primary list-disc my-1 marker:text-muted-foreground marker:text-xs pt-1" {...props} />,
 };
